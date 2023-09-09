219fd3b64b0382a9a660961278e70240437f6731
tohsaka888 <156132264@qq.com>
2023-09-09 19:45:55 +0800

    ✨ feat: add Webview and React

---
+++ esw-jira-client/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../dist/web", // 打包目录
+  },
+});
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { JiraConnector } from "./components/JiraConnector";
+import { JiraTreeProvider } from "./components/SideMenu";
+import { jiraConfig } from "./components/Configure";
+import { jiraPanel } from "./components/WebviewPanel";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,36 +16,19 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "esw-sup-jira.login",
-    async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const baseUrl = await vscode.window.showInputBox({
-        placeHolder: "Please enter your Jira Site Url",
-        value: "",
-      });
-      const email = await vscode.window.showInputBox({
-        placeHolder: "Please enter your Jira bind email",
-        value: "",
-      });
-      const apiToken = await vscode.window.showInputBox({
-        placeHolder: "Please enter your Jira api token",
-        value: "",
-        password: true,
-      });
-      JiraConnector.getInstance({
-        email: email!,
-        baseUrl: baseUrl!,
-        apiToken: apiToken!,
-      });
-      vscode.window.showInformationMessage(
-        `${baseUrl} - ${email} - ${apiToken}`
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  const commandHandlers = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    "esw-sup-jira.login": jiraConfig,
+  };
+  // Register Commands
+  for (const [commandName, commandHandler] of Object.entries(commandHandlers)) {
+    const disposable = vscode.commands.registerCommand(
+      commandName,
+      commandHandler
+    );
+    context.subscriptions.push(disposable);
+  }
+  jiraPanel.webview.postMessage("Hello world");
 }
 
 // This method is called when your extension is deactivated
