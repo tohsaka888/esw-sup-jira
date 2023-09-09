01c9be92a64334f6ffbcb7f6504c53ba9e50a0e4
wangshiyi234 <1790856256@qq.com>
2023-09-09 23:24:07 +0800

    feat: 获取所有项目信息API

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import { Version3Client } from 'jira.js';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -11,5 +12,22 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('string concatenation test', () => {
+		const client = new Version3Client({
+			host: 'https://bitech-automotive.atlassian.net',
+			authentication: {
+				basic: {
+					email: 'shiyi.wang@bitech-automotive.com',
+					apiToken: 'ATATT3xFfGF0R6YVWPzM5ZHyNkKjIxYTNAbRKFGtgRwHwpyIOIlbUNTQA9CntO_0P7rcELMfN1CXEPmP9ndPJFXguZRaHNJ6mWugD-z6YvfnUtC9Qh8P__IOo4TF5uEu_17AUGR0B26Xy7b58C0Q7KGnu-XS-XelX5Aed1TRGn20fyNV2NjQS-s=607B4078',
+				},
+			},
+			newErrorHandling: true,
+		});
+		// async function main() {
+		// 	const projects = await client.projects.searchProjects();
+		// 	console.log(projects);
+		// }
 	});
 });
