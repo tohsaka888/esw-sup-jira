5d981db704d5b98de9cbe81108c55e5b5c00aa06
wangshiyi234 <1790856256@qq.com>
2023-09-10 17:48:35 +0800

    feat: add getAllStatuses and getProjectIssues api

--- src/components/JiraConnector.ts
+++ src/components/JiraConnector.ts
@@ -1,12 +1,12 @@
-import { JiraConnectorProps } from "../types/JiraConnector";
 import { Version3Client } from "jira.js";
 import * as vscode from "vscode";
+import { SearchProjects } from "jira.js/out/version3/parameters";
 
 class JiraConnector {
   private static instance: JiraConnector | null = null;
   private static client?: Version3Client | null = null;
 
-  private constructor() {}
+  private constructor() { }
 
   public static getInstance(): JiraConnector {
     if (!JiraConnector.instance) {
@@ -14,7 +14,25 @@ class JiraConnector {
     }
     return JiraConnector.instance;
   }
-
+  /**
+   * 
+   * @param email 
+   * @param apiToken 
+   * @param url 
+   * @testing
+   * @private
+   */
+  _testJiraConnect(email: string, apiToken: string, url: string) {
+    JiraConnector.client = new Version3Client({
+      host: url,
+      authentication: {
+        basic: {
+          email: email,
+          apiToken: apiToken,
+        },
+      },
+    });
+  }
   // 创建Jira连接器
   async jiraConfig() {
     // The code you place here will be executed every time your command is executed
@@ -50,19 +68,41 @@ class JiraConnector {
     }
   }
 
+
+  //校验登录状态
+  loginAuthenticated() {
+    if (!!JiraConnector.client?.serverInfo) {
+      vscode.window.showErrorMessage("Connection refused,Please try again!");
+    }
+    return !!JiraConnector.client?.serverInfo;
+  }
   // TODO: 获取登录用户参与的所有项目
   // 获取用户相关的所有项目
   async getAllProjects() {
     try {
       return await JiraConnector.client?.projects.searchProjects();
     } catch (error) {
-      vscode.window.showErrorMessage(JSON.stringify(error));
+      vscode.window.showErrorMessage("GetProjects fail,Pleace try again!");
     }
   }
 
-  // TODO: 获取某个项目的所有状态
+  // TODO: 获取项目所有状态
+  async getAllStatuses(projectIdOrKey: string) {
+    try {
+      return await JiraConnector.client?.projects.getAllStatuses(projectIdOrKey);
+    } catch (error) {
+      vscode.window.showErrorMessage("GetStatuses fail,Pleace try again!");
+    }
+  }
 
   // TODO: 获取用户参与的某个项目中的某个状态的事务
+  async getProjectIssues(projectNameOrId: string) {
+    try {
+      return await JiraConnector.client?.issueSearch.searchForIssuesUsingJql({ jql: `project=${projectNameOrId}` });
+    } catch (error) {
+      vscode.window.showErrorMessage("GetIssues fail,Pleace try again!");
+    }
+  }
 }
 
 export const jiraConnector = JiraConnector.getInstance();
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,16 +1,42 @@
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import { jiraConnector } from "../../components/JiraConnector";
 import { expect } from "chai";
 // import * as myExtension from '../../extension';
-
+import { describe, it } from "mocha";
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
-
-  test("Get All API test", async () => {
-    // const con = JiraConnector.getInstance();
-    // const projects = await con.getAllProjects();
-    // console.log(JSON.stringify(projects.values));
-    // expect(projects.values).to.be.an("array");
-  });
+  describe('Get API test', () => {
+    // it('getAllProjects', async () => {
+    //   const con = jiraConnector;
+    //   con._testJiraConnect(
+    //     'diandian.sun@bitech-automotive.com',
+    //     'ATATT3xFfGF0BZ81nsWhSxwcQv6KbEJz83QgTObSL89BlLRy3PsAe1a6V21F8UjZRfHKy9Dbid_KNX2niRLZ3N2i9wrEsn4ZEtF1HNz3_CzojFnCxSPe-9oHE4xk-unLNC8rTNZpUC7WWpU1aXAw2B_8dGA3_C2_xRg_CU5fDj6QnV3Q6BDAXd0=1A860B01',
+    //     'https://bitech-automotive.atlassian.net/'
+    //   )
+    //   const projects = await con.getAllProjects();
+    //   console.log(JSON.stringify(projects?.values));
+    // })
+    // it('getAllStatuses', async () => {
+    //   const con = jiraConnector;
+    //   con._testJiraConnect(
+    //     'diandian.sun@bitech-automotive.com',
+    //     'ATATT3xFfGF0BZ81nsWhSxwcQv6KbEJz83QgTObSL89BlLRy3PsAe1a6V21F8UjZRfHKy9Dbid_KNX2niRLZ3N2i9wrEsn4ZEtF1HNz3_CzojFnCxSPe-9oHE4xk-unLNC8rTNZpUC7WWpU1aXAw2B_8dGA3_C2_xRg_CU5fDj6QnV3Q6BDAXd0=1A860B01',
+    //     'https://bitech-automotive.atlassian.net/'
+    //   )
+    //   const statuses = (await con.getAllStatuses('10003'))!;
+    //   console.log(JSON.stringify(statuses[0]));
+    // })
+    it('getProjectIssues', async () => {
+      const con = jiraConnector;
+      con._testJiraConnect(
+        'diandian.sun@bitech-automotive.com',
+        'ATATT3xFfGF0BZ81nsWhSxwcQv6KbEJz83QgTObSL89BlLRy3PsAe1a6V21F8UjZRfHKy9Dbid_KNX2niRLZ3N2i9wrEsn4ZEtF1HNz3_CzojFnCxSPe-9oHE4xk-unLNC8rTNZpUC7WWpU1aXAw2B_8dGA3_C2_xRg_CU5fDj6QnV3Q6BDAXd0=1A860B01',
+        'https://bitech-automotive.atlassian.net/'
+      )
+      const issues = (await con.getProjectIssues('AiYL_TestProject_ScrumTemplate'))!;
+      console.log(JSON.stringify(issues.issues?.map(issue => issue.id)));
+    })
+  })
 });
